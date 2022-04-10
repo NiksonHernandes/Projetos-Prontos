e8ae8616a7ddc3221c13eabb88bb1a3ee170653c
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-04-10 20:53:10 -0300

    Novo projeto

---
+++ projeto-crudMVC-csharp/Controllers/ProdutosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projeto_crudMVC_csharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_crudMVC_csharp.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        //criando uma lista estática:
+
+        public static List<ProdutosClass> lsProdutos = new List<ProdutosClass>();
+
+
+        public IActionResult Index()
+        {
+            return View(lsProdutos);//informar o model aqui 
+        } 
+
+        public IActionResult CreateProdutos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProdutos(ProdutosClass objeto)
+        {
+            lsProdutos.Add(objeto);
+            return RedirectToAction("Index");
+        }
+    }
+}
